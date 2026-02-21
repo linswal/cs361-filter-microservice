d11fefdd988dc73292138f4d5de0295820e52754
sonja-11 <hanemans@oregonstate.edu>
2026-02-21 13:06:47 -0800

    Implemented validate_request()in microservice.py and test_validation.py

--- microservice.py
+++ microservice.py
@@ -1,3 +1,6 @@
+import json
+
+
 # filter list of items from request_object and return correct list of items
 # items (array) of item (dict)
 # filters (dict)
@@ -5,15 +8,57 @@ def filter_items(items, filters):
     return [
         item for item in items
         if ("category" not in filters or
-            items["category"] == filters["category"])
-        and ("date" not in filters or items["date" == filters["date"]])
+            items.get["category"] == filters["category"])
+        and ("date" not in filters or item.get("date") == filters["date"])
     ]
 
 
 # Feel free to change this function to only return True or False and make another
 # function to write the response object if you'd like!
 def validate_request(request_object):
-    return {}   # or return booleans
+    """
+    Validates JSON request file:
+    - reads JSON
+    - checks if filters are valid
+
+    returns True for filtering
+    returns False for error
+    """
+    errors = []
+
+    # load JSON
+    try:
+        data = json.loads(request_object)
+    except json.JSONDecodeError:
+        return False, ["JSON request is not valid"]
+
+    # check if filter exists and is a dictionary
+    filters = data.get("filters", {})
+    if not isinstance(filters, dict):
+        return False, ["'filters' must be a dictionary"]
+
+    # filter options
+    filter_options = ["category", "date"]
+
+    for key in filters:
+        if key not in filter_options:
+            errors.append(f"Filter not known: '{key}'")
+
+    # check filters are strings
+    if "category" in filters and not isinstance(filters["category"], str):
+        errors.append("'category' must be a string")
+
+    if "date" in filters and not isinstance(filters["date"], str):
+        errors.append("'date' must be a string")
+
+    # check for errors
+    if errors:
+        return False, errors
+
+    return True, filters
+
+
+    # return {}   # or return booleans
 
 
 if __name__ == "__main__":
